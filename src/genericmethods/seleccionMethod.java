6b09da15da818ee1dc26be7e1add9ed10a847966
kevin <kevinaltperez@gmail.com>
2016-05-25 18:57:35 -0500

    metodo1

--- src/genericmethods/seleccionMethod.java
+++ src/genericmethods/seleccionMethod.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package genericmethods;
-
+import java.util.ArrayList;
 /**
  *
  * @author Kevin
  */
 public class seleccionMethod {
+    long tStart,tEnd,tTime;
+    int iteraciones=0;
     
+    public <T extends Comparable<T>> ArrayList<T> selectionSort(ArrayList<T> list){
+            tStart = System.nanoTime(); //de ejecución
+		ArrayList<T> datosOrdenados;
+		datosOrdenados = new ArrayList<T>(list);
+		for (int i =0; i<datosOrdenados.size()-1; i++) {
+			T menor = datosOrdenados.get(i);
+			int pos = i;
+			for (int j = i+1; j < datosOrdenados.size(); j++){
+                            iteraciones++;
+				if(datosOrdenados.get(j).compareTo(menor) < 0) {
+					menor = datosOrdenados.get(j);
+					pos = j;
+				}
+			}
+			datosOrdenados.set(pos,datosOrdenados.get(i));
+			datosOrdenados.set(i,menor);
+		}
+                tEnd = System.nanoTime();
+                tTime = tEnd-tStart;
+		return datosOrdenados;
 }
