968e9d91e707a5070b68de86d44a589f2c1d4ad4
kevin <kevinaltperez@gmail.com>
2016-05-25 19:15:12 -0500

    METODOS AGREGADOS KEVIN

--- src/genericmethods/busquedaSecuencial.java
+++ src/genericmethods/busquedaSecuencial.java
@@ -5,10 +5,64 @@
  */
 package genericmethods;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kevin
  */
 public class busquedaSecuencial {
-    
+    long tStart,tEnd,tTime;
+    int iteraciones=0;
+  
+    public int busquedaSecuencial(ArrayList<Integer> arreglo,int dato){
+        tStart = System.nanoTime(); //de ejecución
+        int posicion = -1;
+        for(int i = 0; i < arreglo.size(); i++){
+            //recorremos todo el arreglo 
+            iteraciones++;
+            int aux = arreglo.get(i);
+            if(aux == dato){//comparamos el elemento en el arreglo con el buscado 
+                posicion = i;//Si es verdadero guardamos la posicion 
+                break;//Para el ciclo 
+            } 
+        } 
+        tEnd = System.nanoTime();
+        tTime = tEnd-tStart;
+        return posicion; 
+    } 
+    public int busquedaSecuencial(ArrayList<Float> arreglo,float dato){
+        tStart = System.nanoTime(); //de ejecución
+        int posicion = -1;
+        for(int i = 0; i < arreglo.size(); i++){
+            //recorremos todo el arreglo 
+            iteraciones++;
+            //Dani lo tiene igual y le agarrar así 
+            float aux = arreglo.get(i);
+            if(aux == dato){//comparamos el elemento en el arreglo con el buscado 
+                posicion = i;//Si es verdadero guardamos la posicion 
+                break;//Para el ciclo 
+            } 
+        } 
+        tEnd = System.nanoTime();
+        tTime = tEnd-tStart;
+        return posicion; 
+    }
+    public int busquedaSecuencial(ArrayList<Character> arreglo,char dato){
+        tStart = System.nanoTime(); //de ejecución
+        int posicion = -1;
+        for(int i = 0; i < arreglo.size(); i++){
+            //recorremos todo el arreglo 
+            iteraciones++;
+            //Dani lo tiene igual y le agarrar así 
+            char aux = arreglo.get(i);
+            if(aux == dato){//comparamos el elemento en el arreglo con el buscado 
+                posicion = i;//Si es verdadero guardamos la posicion 
+                break;//Para el ciclo 
+            } 
+        } 
+        tEnd = System.nanoTime();
+        tTime = tEnd-tStart;
+        return posicion; 
+    }
 }
--- src/genericmethods/quickSortMethod.java
+++ src/genericmethods/quickSortMethod.java
@@ -5,10 +5,54 @@
  */
 package genericmethods;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Kevin
  */
 public class quickSortMethod {
+    long tStart,tEnd,tTime;
+    int iteraciones=0;
+    private ArrayList<T> array;    
+    public <T extends Comparable<T>> ArrayList<T> getArray() {
+        return (ArrayList<T>) this.array;
+    }
+ 
+    public <T extends Comparable<T>> ArrayList<T> quickSort(ArrayList<T> array, int left, int right) {
+        tStart = System.nanoTime(); //TIEMPO DE INICIO DE EJECUCION
+        int inicio = divide(array, left, right); //RECOLECTAMOS EL VALOR ACTUAL       
+        if (left < inicio - 1)//COMPARAMOS UNA POSICION ATRAS
+            quickSort(array, left, inicio - 1);
+        
+        if (inicio < right)
+            quickSort(array, inicio, right);
+        
+        tEnd = System.nanoTime();
+        tTime = tEnd-tStart;
+        return array;
+    }
     
+    public <T extends Comparable<T>> int divide(ArrayList<T> array, int left, int right) {
+        
+        int l = left, r = right;
+        T aux; T pivote = array.get(left);     
+        while (l <= r) {
+            while (array.get(l).compareTo(pivote) < 0)
+                l++;
+            
+            while (array.get(r).compareTo(pivote) > 0)
+                r--;
+            
+            if (l <= r) {
+                iteraciones++;
+                aux = array.get(l);
+                array.set(l, array.get(r));
+                array.set(r, aux);
+                l++;
+                r--;
+            }         
+        };     
+        return l;        
+    }    
 }
